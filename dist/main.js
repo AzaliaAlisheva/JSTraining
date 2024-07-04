74b8defce960921fd954b61072939080b9649276
AzaliaAlisheva <AzaliaAlisheva@users.noreply.github.com>
2024-07-04 07:11:47 +0000

    deploy: d519504ba8ba57ef076e93a278cb0da282c8f857

--- dist/main.js
+++
@@ -1,1 +0,0 @@
-function e(){n.innerHTML="You clicked me"}const n=document.getElementById("contact");n.addEventListener("click",e);export{e as buttonclick};
--- src/main.ts
+++
@@ -1,6 +0,0 @@
-export function buttonclick() {
-    button.innerHTML = "You clicked me";
-}
-
-const button = document.getElementById("contact") as HTMLButtonElement;
-button.addEventListener('click', buttonclick);
